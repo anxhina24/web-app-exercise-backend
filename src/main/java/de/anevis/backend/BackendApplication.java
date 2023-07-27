0c3b77b94af0e17e55d358951cc59395a6aa5440
anxhina <A@nxhina24>
2023-07-27 18:20:25 +0200

    import  GetMapping and RestController

--- src/main/java/de/anevis/backend/BackendApplication.java
+++ src/main/java/de/anevis/backend/BackendApplication.java
@@ -3,6 +3,8 @@ package de.anevis.backend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
 public class BackendApplication {
